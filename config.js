6852989bdf796bf3ef8a83a7e865d6a412b3f4ed
amulanga9 <atyagusova@mail.ru>
2026-09-20 02:05:59 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.CFG = {
-  url: 'https://ВАШ-ID.supabase.co',
-  key: 'sb_publishable_ВАШ_КЛЮЧ'
+  url: 'https://rgblnhjaearokcqsmmzh.supabase.co',
+  key: 'sb_publishable_g0ciM2rdO-6z_WVSnaqTBw_ie1YJFxX'
 };
